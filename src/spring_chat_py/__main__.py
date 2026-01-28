d6a1e84eb3979a5bed3bfd86a32001e0fba343ad
Andreas Bucher <ante.bucher@gmail.com>
2026-01-28 20:45:36 +0100

    d

--- src/spring_chat_py/__main__.py
+++ src/spring_chat_py/__main__.py
@@ -34,18 +34,17 @@ def qdrant_search(query_embedding):
     )
     
     # Search
-    results = client.search(
+    results = client.query_points(
         collection_name=COLLECTION_NAME,
-        query_vector=query_embedding,
+        query=query_embedding,
         limit=5,
         with_payload=True,   # return stored metadata
         with_vectors=False
     )
     
     # Print results
-    for p in results:
-        log.info("Score: %s", p.score)
-        log.info("Payload: %s", p.payload)
+    for p in results.points:
+        log.info("Score: %.3f  Tool: %-20.20s  Description: %s", p.score, p.payload.get("toolName"), p.payload.get("toolDescription").replace("\n", ""))
 
 #
 def qdrant_filter_search(client: QdrantClient, query_embedding: list[float]):
@@ -77,18 +76,33 @@ def get_query_embedding(query_text: str) -> list[float]:
     response.raise_for_status()
     return response.json()["embedding"]
 
-
-def main(argv: list[str] | None = None) -> int: # IGNORE:C0111
-    log.debug("XXXXXXXXXXXXXXXX")
-    log.debug(" spring-chat-py")
-    
-    query_text = "What time is now?"
-    log.debug("query_text: %s", query_text)
+def query_search(query_text: str):
+    log.info("")
+    log.info("query_text: %s", query_text)
+    log.info("")
     
     query_embedding = get_query_embedding(query_text)
-    log.debug("query_text embedding: %s", len(query_embedding))
-    
+    #log.debug("query_text embedding: %s", len(query_embedding))
     qdrant_search(query_embedding)
+    log.info("")
+    log.debug("")
+
+
+def main(argv: list[str] | None = None) -> int: # IGNORE:C0111
+    log.info("XXXXXXXXXXXXXXXX")
+    log.info(" spring-chat-py")
+    
+    query_search("What time is now?")
+    query_search("What day is today?")    
+    query_search("What have been my latest activities?")        
+    query_search("Can you load whoop activities from last week?")  
+    query_search("Can you make a list of all certificates?")
+    
+    query_search("Welche Zeit haben wir?")
+    query_search("Welche Tag ist heute?")    
+    query_search("Was waren die letzen Aktivitäten?")        
+    query_search("Kannst du die Aktivitäten von letzer Woche von Whoop laden?")  
+    query_search("Kannst du die Zertifikate auflisten?")
     
     
 if __name__ == "__main__":
